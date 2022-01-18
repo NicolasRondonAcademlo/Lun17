6f18d3d72b935be5809970dab02152c09bb361b9
Academlo <academlo@MacBook-Pro-de-nicolas.local>
2022-01-17 21:31:07 -0500

    permisos y makepasswors serialzier

--- my_app/views.py
+++ my_app/views.py
@@ -1,14 +1,13 @@
 
-from os import stat
-from re import I
 from django.contrib.auth.models import User
 from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework.response import Response
 from .serializers import UserSerializer, ListUserSerializer, CreateUserSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework import status
+from rest_framework.permissions import IsAdminUser, AllowAny
 # Create your views here.
-
+from django.contrib.auth.hashers import make_password
 
 class UserViewSet(ViewSet):
     def list(self, request):
@@ -46,4 +45,30 @@ class UserViewSet(ViewSet):
 
 class UserModelViewSet(ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    # serializer_class = UserSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return CreateUserSerializer
+
+        elif self.action == 'list':
+            return ListUserSerializer
+        else:
+            return UserSerializer
+
+    # def get_permissions(self):
+    #     if self.action == 'list':
+    #         permission_classes = [AllowAny]
+    #     else:
+    #         permission_classes = [IsAdminUser]
+    #     return [permission() for permission in permission_classes]
+
+
+
+    def perform_create(self, serializer):
+        if ('password' in self.request.data):
+            password = make_password(self.request.data['password'])
+            serializer.save(password=password)
+        else:
+            serializer.save()
+
